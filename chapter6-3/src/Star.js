224bf57116fff7f5e1db7df9e0f3ee21d19f179f
신태림 <str0308@midasit.com>
2019-04-10 19:23:43 +0900

    chapter6-3 끝

---
+++ chapter6-3/src/Star.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Star.css';
+
+// class StarRating extends Component {
+
+// }
+
+const Star = ({selected=false, onClick=f=>f}) => 
+    <div className={(selected) ? 'star selected' : 'star'} onClick={onClick}>
+    </div>;
+
+Star.propTypes = {
+    selected : PropTypes.bool,
+    onClick : PropTypes.func
+};
+
+export default Star;
