fbbb8d4f979e3719b8bb04e1eea9b8b9bc8d08ca
Tae <smireat1@gmail.com>
2019-04-23 00:00:01 +0900

    HOC 체험 하기위한 껍데기

--- chapter7-3/src/index.js
+++ chapter7-3/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PeopleList from './PeopleList';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const RandomUsers = DataComponent(
+    PeopleList,
+    'https://randomuser.me/api'
+)
+
+ReactDOM.render(<RandomUsers count={10} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
