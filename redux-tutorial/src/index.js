fb2e633632ed14cc52cddb4a23d34e794c27721e
Tae <smireat1@gmail.com>
2019-06-19 00:33:19 +0900

    it's just a redux-example not a react-redux example

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -18,8 +18,9 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store/index";
-import App from "./components/App.jsx";
 // if you're in create-react-app import the files as:
  import store from "./js/store/index";
 // import App from "./js/components/App.jsx";
+import {addArticle} from './js/actions/index'
+window.store = store;
+window.addArticle =addArticle;
